b5cfbe2228c73ca695f423b26267f260295b3ca9
José Franco Neto <jfranconeto90@gmail.com>
2019-09-07 01:45:56 -0300

    Fix request.js

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,23 +1,27 @@
 import axios from 'axios'
 import config from '../../configs'
 import log4js from 'log4js'
+import {get} from 'lodash'
 const log = log4js.getLogger('request')
 axios.defaults.baseURL = config.github.host;
 
 export default async (params) => {
+    log.info(config.github.host)
     try {
         const response = await axios(params)
         return response
     } catch (err) {
         log.error(err.message)
-        switch (err.response.status) {
-            case 403:
-                throw new Error('ERROR:FORBIDDEN')
-            case 404:
-                throw new Error('ERROR:NOT_FOUND')
-            default:
-                throw new Error('ERROR:DEFAULT')
-
+        if (get(err, 'response', false)) {
+            switch (err.response.status) {
+                case 403:
+                    throw new Error('ERROR:FORBIDDEN')
+                case 404:
+                    throw new Error('ERROR:NOT_FOUND')
+                default:
+                    throw new Error('ERROR:DEFAULT')
+            }
         }
+        throw new Error('ERROR:DEFAULT')
     }
 }
