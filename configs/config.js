97365128c85b3b81908e13649b07c2c6536378a8
José Franco Neto <jfranconeto90@gmail.com>
2019-09-07 01:36:20 -0300

    Update port

--- configs/config.js
+++ configs/config.js
@@ -5,7 +5,7 @@ const config = {
     node_env: process.env.NODE_ENV,
     api: {
         env: process.env.NODE_ENV,
-        port: process.env.API_PORT,
+        port: process.env.PORT || process.env.API_PORT,
         host: process.env.API_HOST
     },
     github: {
